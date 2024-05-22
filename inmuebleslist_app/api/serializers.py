66ac52bc8ece7a5864645ab000ac7a4bc78ae1e1
fernandoSoteloRomero <al169888@alumnos.uacj.mx>
2024-05-22 15:44:02 -0600

    wip

--- inmuebleslist_app/api/serializers.py
+++ inmuebleslist_app/api/serializers.py
@@ -1,22 +1,116 @@
 from rest_framework import serializers
-from inmuebleslist_app.models import Inmueble
+from inmuebleslist_app.models import Inmueble, Empresa
 
-class InmuebleSerializer(serializers.Serializer):
-  id = serializers.IntegerField(read_only = True)
-  direccion = serializers.CharField()
-  pais = serializers.CharField()
-  descripcion = serializers.CharField()
-  imagen = serializers.CharField()
-  active = serializers.BooleanField()
+class EmpresaSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Empresa
+    fields = "__all__"
+
+
+
+
+class InmuebleSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Inmueble
+    fields = "__all__"
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # fields = ['id', 'pais', 'active', 'imagen']
   
-  def create(self, validated_data):
-    return Inmueble.objects.create(**validated_data)
+  # def validate(self, data):
+  #   if data['direccion']==data['pais']:
+  #     raise serializers.ValidationError('La direccion y el pais deben ser diferentes')
+  #   else:
+  #     return data
   
-  def update(self, instance, validated_data):
-    instance.direccion = validated_data.get('direccion', instance.direccion)
-    instance.pais = validated_data.get('pais', instance.pais)
-    instance.descripcion = validated_data.get('descripcion', instance.descripcion)
-    instance.imagen = validated_data.get('imagen', instance.imagen)
-    instance.active = validated_data.get('active', instance.active)
-    instance.save()
-    return instance
+  # def validate_imagen(self, data):
+  #   if len(data) <= 2:
+  #     raise serializers.ValidationError('La url de la imagen es muy corta')
+  #   else:
+  #     return data   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+# def column_longitud(value):
+#   if len(value) <= 2:
+#     raise serializers.ValidationError('El valor es demasiado corto')
+
+
+
+# class InmuebleSerializer(serializers.Serializer):
+#   id = serializers.IntegerField(read_only = True)
+#   direccion = serializers.CharField(validators = [column_longitud])
+#   pais = serializers.CharField()
+#   descripcion = serializers.CharField()
+#   imagen = serializers.CharField()
+#   active = serializers.BooleanField()
+  
+#   def create(self, validated_data):
+#     return Inmueble.objects.create(**validated_data)
+  
+#   def update(self, instance, validated_data):
+#     instance.direccion = validated_data.get('direccion', instance.direccion)
+#     instance.pais = validated_data.get('pais', instance.pais)
+#     instance.descripcion = validated_data.get('descripcion', instance.descripcion)
+#     instance.imagen = validated_data.get('imagen', instance.imagen)
+#     instance.active = validated_data.get('active', instance.active)
+#     instance.save()
+#     return instance
+  
+#   def validate(self, data):
+#     if data['direccion']==data['pais']:
+#       raise serializers.ValidationError('La direccion y el pais deben ser diferentes')
+#     else:
+#       return data
+    
+#   def validate_imagen(self, data):
+#     if len(data) <= 2:
+#       raise serializers.ValidationError('La url de la imagen es muy corta')
+#     else:
+#       return data
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- inmuebleslist_app/models.py
+++ inmuebleslist_app/models.py
@@ -8,6 +8,19 @@ class Inmueble(models.Model):
   descripcion = models.CharField(max_length=500)
   imagen = models.CharField(max_length=500)
   active = models.BooleanField(default=True)
+  created = models.DateTimeField(auto_now_add=True)
+  
+  
   
   def __str__(self):
     return self.descripcion
+
+class Empresa(models.Model):
+  nombre = models.CharField(max_length=250)
+  website = models.URLField(max_length=250)
+  active = models.BooleanField(default=True)
+  
+  
+  
+  def __str__(self):
+    return self.nombre
